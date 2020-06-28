9933d2a7889b70c48e0e21b5afef883c0db7f877
Andre2610 <dre2610@gmail.com>
2020-06-28 16:19:29 +0200

    auth, signup and login features added. not test yet

---
+++ auth/middleware.js
@@ -0,0 +1,33 @@
+const User = require("../models").user;
+const { toData } = require("./jwt");
+
+async function auth(req, res, next) {
+  //   console.log("Do I get called?");
+  try {
+    const authHeader =
+      req.headers.authorization && req.headers.authorization.split(" ");
+    // console.log("What is in my header", authHeader);
+    if (authHeader && authHeader[0] === "Bearer" && authHeader[1]) {
+      try {
+        const data = toData(authHeader[1]);
+        // console.log("What is in data", data);
+        const user = await User.findByPk(data.userId);
+        // console.log("Who is the user", user);
+        if (!user) {
+          next();
+        } else {
+          req.user = user;
+          next();
+        }
+      } catch (e) {
+        res.status(400).send("Invalid JWT token");
+      }
+    } else {
+      res.status(401).send({ message: "Please supply valid credentials" });
+    }
+  } catch (e) {
+    next(e);
+  }
+}
+
+module.exports = auth;
